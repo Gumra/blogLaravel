04cc2c3ea3d53c09d1f66fdb00f0a74042d3ce0d
Дмитрий <shapovalov.dmit@inbox.ru>
2022-06-05 18:12:47 +0300

    Реализация CRUD для категорий

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,81 @@
+@extends('admin.layouts.main')
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Категории</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-1 mb-2">
+                        <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Создать</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card">
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr class="align-items-center text-center">
+                                        <th>ID</th>
+                                        <th>Название</th>
+                                        <th colspan="3" class="text-center">Действие</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($categories as $category)
+                                        <tr class="text-center">
+                                            <td>{{ $category->id }}</td>
+                                            <td>{{ $category->title }}</td>
+                                            <td>
+                                                <a href="{{ route('admin.category.show',$category->id) }}">
+                                                    <i class="far fa-eye"></i>
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <a href="{{ route('admin.category.edit',$category->id) }}">
+                                                    <i class="fas fa-pencil-alt"></i>
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <form action="{{ route('admin.category.delete',$category->id) }}"
+                                                      method="post">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button type="submit" class="border-0 bg-transparent">
+                                                        <i class="fas fa-trash" role="button"></i>
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
